57c0d88d1e5f07ad79bd516de84db30fb403f155
dayana <dayana.ruiz@inetum.com>
2025-05-20 18:57:51 +0200

    test incremento y decremento botón

--- contador-app/src/__tests__/App.test.tsx
+++ contador-app/src/__tests__/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Selector Numérico - Funcionalidad Básica', () => {
@@ -7,5 +8,25 @@ describe('Selector Numérico - Funcionalidad Básica', () => {
     render(<App />);
     const counterValue = screen.getByText('5'); // Valor inicial por defecto
     expect(counterValue).toBeInTheDocument();
+  });
+});
+
+describe('Botón incremento - Funcionalidad Básica', () => {
+  test('Comprueba incremento mostrado mediante un botón', async () => {
+    render(<App />);
+    const counterValue = screen.getByText('5'); // Valor inicial por defecto
+    const button = screen.getByText('+')
+    await userEvent.click(button);
+    expect(counterValue).toHaveTextContent("6");
+  });
+});
+
+describe('Botón decremento - Funcionalidad Básica', () => {
+  test('Comprueba que se decrementa valor mostrado mediante un botón', async () => {
+    render(<App />);
+    const counterValue = screen.getByText('5'); // Valor inicial por defecto
+    const button = screen.getByText('-')
+    await userEvent.click(button);
+    expect(counterValue).toHaveTextContent("4");
   });
 });
